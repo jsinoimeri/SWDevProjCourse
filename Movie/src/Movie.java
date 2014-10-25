49fc9f0381cba0a770f4805e8e1cb01179616a3d
Jeton <jsinoimeri@yahoo.ca>
2014-10-25 13:30:46 -0400

    Version 1.1 of Movie

--- Movie/src/Movie.java
+++ Movie/src/Movie.java
@@ -1,13 +1,42 @@
+/**
+ * Movie class representing a Movie Object
+ * that contains only a name.
+ * 
+ * 
+ * @author Jeton Sinoimeri
+ * 
+ * @version 1.1
+ * @created Oct 23, 2014
+ * @modified Oct 25, 2014
+ *
+ */
 
 public class Movie 
 {
 
-	private String name;
+	private String name;            // String instance representing name of movie
+	
+	
+	/**
+	 * Contructor for the Movie class
+	 * 
+	 * @param name -> String object representing the name
+	 *                of the Movie.
+	 *                
+	 */
 	
 	public Movie (String name)
 	{
 		this.name = name;
 	}
+	
+	
+	/**
+	 * Accessor for the name of the Movie.
+	 * 
+	 * @return name -> String object representing the name
+	 *                 of the Movie.
+	 */
 	
 	public String getName()
 	{
--- Movie/src/MovieList.java
+++ Movie/src/MovieList.java
@@ -4,23 +4,58 @@ import java.util.ArrayList;
 public class MovieList 
 {
 	
-	
+	// ArrayList object to maintain a list of Movies
 	private ArrayList<Movie> movieList;
+	
+	
+	/**
+	 * Constructor for the MovieList class
+	 * 
+	 */
 	
 	public MovieList()
 	{
 		this.movieList = new ArrayList<Movie>();
 	}
 	
+	
+	/**
+	 * Accessor for the size of the list of movies.
+	 * 
+	 * @return size -> integer representing the size of
+	 *                 the movie list.
+	 *                 
+	 */
+	
 	public int getSize()
 	{
 		return this.movieList.size();
 	}
 	
+	
+	/**
+	 * Adds a movie to the list.
+	 * 
+	 * @param movie -> Movie object representing the movie to
+	 *                 be added to the list.
+	 *                 
+	 */
+	
 	public void add(Movie movie)
 	{
 		this.movieList.add(movie);
 	}
+	
+	
+	/**
+	 * Checks if a movie is in the list or not.
+	 * 
+	 * @param movie -> Movie object to be checked if it is in
+	 *                 the list.
+	 * @return value -> boolean value representing if a movie
+	 *                  is in the list or not.
+	 *                  
+	 */
 	
 	public boolean contains(Movie movie)
 	{
